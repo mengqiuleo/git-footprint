34dab8e00a77c612e598fc0feb8ff93d866d2a31
潘静怡 <1003346758@qq.com>
2025-09-27 19:14:36 +0800

    feat: 解析命令行参数

--- src/cli.rs
+++ src/cli.rs
@@ -5,7 +5,7 @@ use anyhow::Result;
 
 #[derive(Parser, Debug)]
 #[command(name = "git-stats")]
-#[command(about = "Generate Git contribution stats.", long_about = None)]
+#[command(about, version)]
 pub struct CliArgs {
     /// Git user email to filter commits
     #[arg(short, long)]
@@ -15,24 +15,41 @@ pub struct CliArgs {
     #[arg(short, long, default_value = ".")]
     pub path: String,
 
-    /// Start date (YYYY-MM-DD)
-    #[arg(long)]
-    pub since: Option<NaiveDate>,
+    /// Start date (YYYY-MM-DD), defaults to first day of current year, supported formats are YYYY-MM-DD, YYYY/MM/DD, YYYY.MM.DD
+    #[arg(short = 's', long = "start", value_parser = parse_naive_date)]
+    pub start_date: Option<NaiveDate>,
 
-    /// End date (YYYY-MM-DD)
-    #[arg(long)]
-    pub until: Option<NaiveDate>,
+    /// End date (YYYY-MM-DD), defaults to last day of current year
+    #[arg(short = 'e', long = "end", value_parser = parse_naive_date)]
+    pub end_date: Option<NaiveDate>,
+}
+
+
+fn parse_naive_date(s: &str) -> Result<NaiveDate, String> {
+    let formats = [
+        "%Y-%m-%d",    // 2024-01-01
+        "%Y/%m/%d",    // 2024/01/01
+        "%Y.%m.%d",    // 2024.01.01
+    ];
+
+    for fmt in formats.iter() {
+        if let Ok(date) = NaiveDate::parse_from_str(s, fmt) {
+            return Ok(date);
+        }
+    }
+
+    Err(format!("无法解析日期: '{}'，支持的格式: YYYY-MM-DD, YYYY/MM/DD, YYYY.MM.DD", s))
 }
 
 impl CliArgs {
     pub fn get_date_range(&self) -> Result<(NaiveDate, NaiveDate)>  {
         let current_year = Local::now().year();
 
-        let since = self.since.unwrap_or_else(|| {
+        let since = self.start_date.unwrap_or_else(|| {
             NaiveDate::from_ymd_opt(current_year, 1, 1).unwrap()
         });
 
-        let until = self.until.unwrap_or_else(|| {
+        let until = self.end_date.unwrap_or_else(|| {
             NaiveDate::from_ymd_opt(current_year, 12, 31).unwrap()
         });
 
